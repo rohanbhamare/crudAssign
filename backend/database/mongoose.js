c349e3916db2c3396186ec53e3e0653933834a1c
rohanbhamare <rohan.bhamare@kinai.aero>
2020-12-05 00:00:54 +0530

    updated

--- backend/database/mongoose.js
+++ backend/database/mongoose.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 
+mongoose.Promise = global.Promise;
 mongoose.connect("mongodb://localhost:27017/usersDb", {useNewUrlParser:true,useUnifiedTopology:true}, ()=>{
     console.log(" mongo database connection succeded...")
 });
